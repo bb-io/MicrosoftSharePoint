8d10977e78ba60da3fa64076b7caa222a7f51cbf
RiabushenkoA <ariabushenko@blackbird.io>
2025-11-06 16:44:47 +0200

    Added Folder pikcer handler

--- Apps.MicrosoftSharePoint/Webhooks/Inputs/FolderInput.cs
+++ Apps.MicrosoftSharePoint/Webhooks/Inputs/FolderInput.cs
@@ -1,12 +1,13 @@
 ﻿using Apps.MicrosoftSharePoint.DataSourceHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems;
 
 namespace Apps.MicrosoftSharePoint.Webhooks.Inputs;
 
 public class FolderInput
 {
-    [Display("Folder ID")] 
-    [DataSource(typeof(FolderDataSourceHandler))]
+    [Display("Folder ID")]
+    [FileDataSource(typeof(FolderPickerDataSourceHandler))]
     public string? ParentFolderId { get; set; }
 }
