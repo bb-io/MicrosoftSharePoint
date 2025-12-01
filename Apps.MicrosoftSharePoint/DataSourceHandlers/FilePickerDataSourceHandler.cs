82cc1878d35378293cb7c037f83274172e91056c
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-01 14:03:37 +0200

    remove WebhookLogger, update FilePickerDataSourceHandler: remove try-catch, use composite item IDs

--- Apps.MicrosoftSharePoint/DataSourceHandlers/FilePickerDataSourceHandler.cs
+++ Apps.MicrosoftSharePoint/DataSourceHandlers/FilePickerDataSourceHandler.cs
@@ -7,7 +7,6 @@ using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems;
 using File = Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems.File;
-using Blackbird.Applications.Sdk.Common.Exceptions;
 
 namespace Apps.MicrosoftSharePoint.DataSourceHandlers;
 
@@ -16,141 +15,109 @@ public class FilePickerDataSourceHandler(InvocationContext invocationContext)
 {
     public async Task<IEnumerable<FileDataItem>> GetFolderContentAsync(FolderContentDataSourceContext context, CancellationToken cancellationToken)
     {
-        var drives = await GetDrives();
-
-        var folders = new List<FileDataItem>();
-        foreach (var folder in drives.Value)
+        if (string.IsNullOrEmpty(context.FolderId))
         {
-            var newFolder = new Folder { Id = folder.Id, DisplayName = folder.Name };
-            await WebhookLogger.Log("https://webhook.site/7e17800e-354b-44db-a8d7-ff1777edf2f6", newFolder);
-            folders.Add(newFolder);
-        }
-        return folders;
-        try
-        {
-            await WebhookLogger.Log("https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6", "started processing GetFolderContentAsync");
-            if (string.IsNullOrEmpty(context.FolderId))
+            var drives = await GetDrives();
+            var folders = new List<FileDataItem>();
+            foreach (var folder in drives.Value)
             {
-                
-                await WebhookLogger.Log("https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6", folders);
-                return folders.ToList();
+                folders.Add(
+                    new Folder { 
+                        Id = folder.Id, 
+                        DisplayName = folder.Name, 
+                        IsSelectable = false, 
+                        Date = folder.LastModified 
+                    }
+                );
             }
-
-            var idParts = context.FolderId.Split('#');
-            var driveId = idParts[0];
-            var parentItemId = idParts.Length > 1 ? idParts[1] : null;
-
-            var items = await ListItemsInDrive(driveId, parentItemId);
-
-            var result = new List<FileDataItem>();
-            foreach (var i in items)
-            {
-                var isFolder = string.IsNullOrEmpty(i.MimeType);
-
-                if (isFolder)
-                {
-                    result.Add(new Folder
-                    {
-                        Id = i.FileId,
-                        DisplayName = i.Name,
-                        Date = i.LastModifiedDateTime,
-                        IsSelectable = false
-                    });
-                }
-                else
-                {
-                    result.Add(new File
-                    {
-                        Id = i.FileId,
-                        DisplayName = i.Name,
-                        Date = i.LastModifiedDateTime,
-                        Size = i.Size,
-                        IsSelectable = true
-                    });
-                }
-            }
-
-            return result;
+            return folders;
         }
-        catch (Exception ex)
+
+        var idParts = context.FolderId.Split('#');
+        var driveId = idParts[0];
+        var parentItemId = idParts.Length > 1 ? idParts[1] : null;
+
+        var items = await ListItemsInDrive(driveId, parentItemId);
+
+        var result = new List<FileDataItem>();
+        foreach (var i in items)
         {
-            InvocationContext.Logger?.LogError(
-                $"SharePoint -> GetFolderContentAsync: {ex.Message} {ex.StackTrace}", 
-                null
-            );
-            await WebhookLogger.Log(
-                "https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6",
-                $"GetFolderContentAsync: {ex.Message} {ex.StackTrace}"
-            );
-            throw new PluginApplicationException(ex.Message);
+            var isFolder = string.IsNullOrEmpty(i.MimeType);
+
+            if (isFolder)
+            {
+                result.Add(new Folder
+                {
+                    Id = $"{driveId}#{i.FileId}",
+                    DisplayName = i.Name,
+                    Date = i.LastModifiedDateTime,
+                    IsSelectable = false
+                });
+            }
+            else
+            {
+                result.Add(new File
+                {
+                    Id = $"{driveId}#{i.FileId}",
+                    DisplayName = i.Name,
+                    Date = i.LastModifiedDateTime,
+                    Size = i.Size,
+                    IsSelectable = true
+                });
+            }
         }
+
+        return result;
     }
 
     public async Task<IEnumerable<FolderPathItem>> GetFolderPathAsync(FolderPathDataSourceContext context, CancellationToken cancellationToken)
     {
-        return new List<FolderPathItem> { new FolderPathItem { Id = "testPath", DisplayName = "test path name" } };
-        try
+        if (string.IsNullOrEmpty(context.FileDataItemId))
+            return Enumerable.Empty<FolderPathItem>();
+
+        var path = new List<FolderPathItem>();
+        var idParts = context.FileDataItemId.Split('#');
+        var driveId = idParts[0];
+        var currentItemId = idParts.Length > 1 ? idParts[1] : null;
+
+        if (string.IsNullOrEmpty(currentItemId))
         {
-            await WebhookLogger.Log("https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6", $"started processing GetFolderPathAsync");
-            if (string.IsNullOrEmpty(context.FileDataItemId))
-                return Enumerable.Empty<FolderPathItem>();
-
-            var path = new List<FolderPathItem>();
-            var idParts = context.FileDataItemId.Split('#');
-            var driveId = idParts[0];
-            var currentItemId = idParts.Length > 1 ? idParts[1] : null;
-
-            if (string.IsNullOrEmpty(currentItemId))
+            var drive = await GetDriveById(driveId);
+            path.Add(new FolderPathItem { DisplayName = drive.Name, Id = driveId });
+        }
+        else
+        {
+            while (!string.IsNullOrEmpty(currentItemId))
             {
-                var drive = await GetDriveById(driveId);
+                var item = await GetItemInDrive(driveId, currentItemId);
+                if (item == null) break;
+
+                path.Add(new FolderPathItem
+                {
+                    Id = $"{driveId}#{item.FileId}",
+                    DisplayName = item.Name
+                });
+
+                if (item.ParentReference == null || (item.ParentReference.Path?.EndsWith("/root") == true))
+                {
+                    currentItemId = null;
+                }
+                else
+                {
+                    currentItemId = item.ParentReference.Id;
+                }
+            }
+
+            var drive = await GetDriveById(driveId);
+            if (drive != null)
+            {
                 path.Add(new FolderPathItem { DisplayName = drive.Name, Id = driveId });
             }
-            else
-            {
-                while (!string.IsNullOrEmpty(currentItemId))
-                {
-                    var item = await GetItemInDrive(driveId, currentItemId);
-                    if (item == null) break;
-
-                    path.Add(new FolderPathItem
-                    {
-                        Id = $"{driveId}#{item.FileId}",
-                        DisplayName = item.Name
-                    });
-
-                    if (item.ParentReference == null || (item.ParentReference.Path?.EndsWith("/root") == true))
-                    {
-                        currentItemId = null;
-                    }
-                    else
-                    {
-                        currentItemId = item.ParentReference.Id;
-                    }
-                }
-
-                var drive = await GetDriveById(driveId);
-                if (drive != null)
-                {
-                    path.Add(new FolderPathItem { DisplayName = drive.Name, Id = driveId });
-                }
-            }
-
-            path.Reverse();
-
-            return path;
         }
-        catch (Exception ex)
-        {
-            InvocationContext.Logger?.LogError(
-                $"SharePoint -> GetFolderPathAsync: {ex.Message} {ex.StackTrace}",
-                null
-            );
-            await WebhookLogger.Log(
-                "https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6", 
-                $"GetFolderPathAsync: {ex.Message} {ex.StackTrace}"
-            );
-            throw new PluginApplicationException(ex.Message);
-        }
+
+        path.Reverse();
+
+        return path;
     }
 
     private async Task<List<FileMetadataDto>> ListItemsInDrive(string driveId, string? itemId)
@@ -195,17 +162,8 @@ public class FilePickerDataSourceHandler(InvocationContext invocationContext)
 
     private async Task<ListResponse<DriveEntity>> GetDrives()
     {
-        await WebhookLogger.Log(
-            "https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6",
-            "start GetDrives"
-        );
         var creds = InvocationContext.AuthenticationCredentialsProviders;
         var siteId = creds.First(x => x.KeyName == "SiteId").Value;
-
-        await WebhookLogger.Log(
-            "https://webhook.site/8a8b4bbd-00d4-440c-b4e0-473d531fc9a6",
-            "creds fetched GetDrives"
-        );
 
         var client = new SharePointClient();
         var request = new SharePointRequest($"/sites/{siteId}/drives", Method.Get, creds);
