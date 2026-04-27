6e4525b8f6cb0c341eac073477d78bc3dd1e8402
mathijs-bb <mathijs.sonnemans@blackbird.io>
2026-04-27 21:06:54 +0200

    Added file name sanitization

---
+++ Apps.MicrosoftSharePoint/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Apps.MicrosoftSharePoint.Extensions;
+public static class StringExtensions
+{
+    public static string SanitizeFileName(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        // Replace invalid SharePoint characters with underscore
+        string invalidCharsPattern = @"[""*:<>\?\/\\|#%]";
+        string sanitized = Regex.Replace(input, invalidCharsPattern, "_");
+
+        sanitized = sanitized.Trim();
+        sanitized = sanitized.TrimEnd('.');
+
+        return sanitized;
+    }
+}
